096205c76bb14ed83cc43bd133a31f707730c2a3
garotolinux-oss <garotolinux@gmail.com>
2026-04-24 00:32:01 -0300

    ejs

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+//import bodyParser from "body-parser";
+//import express from "express"
+const bodyParser = require("body-parser")
 const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path")
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.render('index.ejs')
 });
 
-app.get("/create_page", (req,res) => {
-    res.sendFile(path.join(__dirname,"create_page.html" ))
+app.get("/create_page.ejs", (req,res) => {
+    res.render('create_page.ejs')
 });
+
+app.get("/index.ejs", (req,res) => {
+    res.render("index.ejs")
+});
+
+let blogs = []
+let post = {titulo: "",
+    conteudo: ""
+    
+}
+app.post("/submit", (req, res, next) => {
+   
+    let post = req.body 
+    post.titulo = req.body.titulo_in
+    post.conteudo = req.body.corpo
+    blogs.push(post)
+    res.render("index.ejs", {blogs: blogs})
+    
+})
+
 
 app.listen(port, () => {
     console.log("sevidor rodando na porta" + port);
